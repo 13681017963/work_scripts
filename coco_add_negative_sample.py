09ac803adff410c1a0ed32f1de73996c8950ec1c
zyan <386980561@qq.com>
2022-11-17 14:54:05 +0800

    20221115 增加coco添加负样本脚本

--- coco_add_negative_sample.py
+++ coco_add_negative_sample.py
@@ -1,22 +1,30 @@
 import glob
-import json
+import mmcv
 import sys
-import cv2
-import tqdm
 from IPython import embed
 
+
+# sys[1] 负样本图片文件夹
+# sys[2] 原coco文件路径
+# sys[3] 添加负样本后的输出文件
 jpgs = glob.glob("%s/*.jpg" % sys.argv[1])
 jpegs = glob.glob("%s/*.jpeg" % sys.argv[1])
 pngs = glob.glob("%s/*.png" % sys.argv[1])
 files = jpgs + jpegs + pngs
-names = []
-id = int(sys.argv[2])
-for file in tqdm.tqdm(files):
+add_info = mmcv.load(sys.argv[2])
+id = -1
+for i in add_info['images']:
+    id = max(id, i['id'])
+id += 1
+coco_out_json = sys.argv[3]
+
+for file in mmcv.track_iter_progress(files):
+    file = file.replace('\\', '/')
     jpg_name = file.split('/')[-1]
-    img = cv2.imread(file)
+    img = mmcv.imread(file)
     height = img.shape[0]
     width = img.shape[1]
     tmp = {"id": id, "file_name": jpg_name, "height": height, "width": width}
-    names.append(tmp)
+    add_info['images'].append(tmp)
     id += 1
-print(json.dumps(names, ensure_ascii=False))
+mmcv.dump(add_info, coco_out_json, indent=4)
